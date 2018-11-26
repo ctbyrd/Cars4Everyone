46b0f475df59ae338b2c90e2ddff8532027506c8
bhiggi12 <42845468+bhiggi12@users.noreply.github.com>
2018-11-26 14:02:06 -0500

    Added database information file
    Used to interact with the database, update file locally on computer due to information contained

---
+++ php/database.php
@@ -0,0 +1,8 @@
+<!-- Need to fill in information for database location
+  -->
+<?php
+$servername = ; //insert servername
+$username = ; //insert username
+$password = ; //insert password
+$dbname = 'Cars4Everyone_DB';
+?>
