9a0b88dc6b129db62a615514c080853057f96056
Luan Pham <luanpham.aca@gmail.com>
2018-11-29 22:31:45 -0500

    ahhhhh

--- php/database.php
+++ php/database.php
@@ -2,6 +2,6 @@
   -->
 <?php
 $servername = c4e1db.ccqveog3ruej.us-east-1.rds.amazonaws.com; //insert servername
-$username = "root"; //insert username
-$password = "Cars4Everyone!"; //insert password
+$username = ""; //insert username
+$password = ""; //insert password
 ?>
