66e2425388fa159f9c53a779532807fe2d96d2fa
bhiggi12 <42845468+bhiggi12@users.noreply.github.com>
2018-12-02 21:07:07 -0500

    Updated index.php
    Changed the file to work with a iframe for results

--- index.php
+++ index.php
@@ -8,34 +8,22 @@
     </style>
 
     <!-- External Styling Sheets -->
-    <!--<link href="./css/Cars4Everyone_Styles.css" type="text/css" rel="stylesheet"/>-->
     <link href="./css/GeneralLayout.css" type="text/css" rel="stylesheet"/>
     <link href="./css/SearchPanel.css" type="text/css" rel="stylesheet"/>
     <link href="./css/ResultsPanelHeader.css" type="text/css" rel="stylesheet"/>
-    <link href="./css/ResultsPanelResults.css" type="text/css" rel="stylesheet"/>
 
   </head>
 
-  <!-- External Javascript -->
-  <script src="./javascript/dropdowns.js"></script>
-
   <body>
     <!-- Files with php Functions -->
-    <!-- TO DO:
-         Consolidate into one file (?)
-      -->
     <?php
       //Function to populate search panel options
       include 'php/searchPanelPopulation.php';
-      //Function to populate results panel
-      include 'php/resultsPanelPopulation.php';
       //Variable to hold the current query
-
       $curr_query = "SELECT * from car";
     ?>
     <!-- Top Area -->
     <div id = "PageHeader">
-      <!-- Possibly replace with a logo image -->
       <div id = "TopLogo">Cars4Everyone</div>
     </div>
     <hr/>
@@ -48,131 +36,96 @@
         <!-- Portions with Buttons to Signal the User Wants the Query Changed -->
         <div id = "BodyPanelButtons">
           <!-- TO DO: Positioning of this line -->
-          <!-- TO DO: Trigger Event -> Add Selected Parameters to Query and
-               Send it to the Database to Receive Results
-               $curr_query =
-               "SELECT * FROM cars
-                WHERE variable1 in (options1) &&
-                      variable2 in (options2)"
-             -->
-          <input type= 'submit' value="submit" id = "SearchButton" class = "PanelHeader" >Search</input>
-          <!-- TO DO: Trigger Event -> Remove Parameters and
-               Request All Cars from the Database
-               New SQL: "SELECT * FROM cars;"
-            -->
-          <button id = "ResetButton" class = "PanelHeader">Reset</button>
+          <form name="form" id="submitButtonForm" method="POST" action="php/resultsPanelPopulation.php" target="results_panel">
+            <input type= 'submit' value="Search" id = "SearchButton" class = "PanelHeader"></input>
+            <!-- TO DO: Trigger Event -> Remove Parameters and
+                Request All Cars from the Database
+                New SQL: "SELECT * FROM cars;"
+              -->
+            <button id = "ResetButton" class = "PanelHeader">Reset</button>
         </div>
         <!-- Collapsing Filters for Results -->
         <div id = "FilterContainer">
           <!-- TO DO: Get values that are selected-->
-          <button class="accordion" id = "MakeFilter">Make</button>
+          <button type="button" class="accordion" id = "MakeFilter">Make</button>
           <!-- Populates results using php -->
-          <form name= "makeF" target= "cframe" method="post" action="index.php"  class="panel" id="MakeFilterOptions">
-            <ul>
-              <?php
-                PopulateSearchPanel("make", $curr_query);
-              ?>
-            </ul>
-          </form>
-          <button class="accordion" id = "ModelFilter">Model</button>
+          <ul>
+            <?php
+              echo PopulateSearchPanel("make", $curr_query);
+            ?>
+          </ul>
+          <button type="button" class="accordion" id = "YearFilter">Year</button>
           <!-- Populates results using php -->
-          <form name="modelF" method="post" action="index.php"  class="panel" id="ModelFilterOptions">
-            <ul>
-              <?php
-                PopulateSearchPanel("model", $curr_query);
-              ?>
-            </ul>
-          </form>
-          <button class="accordion" id = "YearFilter">Year</button>
+          <ul>
+            <?php
+              echo PopulateSearchPanel("year", $curr_query);
+            ?>
+          </ul>
+          <button type="button" class="accordion" id = "PriceFilter">Price</button>
           <!-- Populates results using php -->
-          <form name="yearF" method="post" action="index.php"  class="panel" id="YearFilterOptions">
-            <ul>
-              <?php
-                PopulateSearchPanel("year", $curr_query);
-              ?>
-            </ul>
-          </form>
-          <button class="accordion" id = "PriceFilter">Price</button>
+          <ul>
+            <?php
+              echo PopulateSearchPanel("price", $curr_query);
+            ?>
+          </ul>
+          <button type="button" class="accordion" id = "CityMPGFilter">City MPG</button>
           <!-- Populates results using php -->
-          <form name="priceF" method="post" action="index.php"  class="panel" id="PriceFilterOptions">
-            <ul>
-              <?php
-                PopulateSearchPanel("price", $curr_query);
-              ?>
-            </ul>
-          </form>
-          <button class="accordion" id = "CityMPGFilter">City MPG</button>
+          <ul>
+            <?php
+              echo PopulateSearchPanel("MPG_city", $curr_query);
+            ?>
+          </ul>
+          <button type="button" class="accordion" id = "HighwayMPGFilter">Highway MPG</button>
           <!-- Populates results using php -->
-          <form name="MPG_city" method="post" action="index.php"  class="panel" id="CityMPGFilterOptions">
-            <ul>
-              <?php
-                PopulateSearchPanel("MPG_city", $curr_query);
-              ?>
-            </ul>
-          </form>
-          <button class="accordion" id = "HighwayMPGFilter">Highway MPG</button>
+          <ul>
+            <?php
+              echo PopulateSearchPanel("MPG_highway", $curr_query);
+            ?>
+          </ul>
+          <button type="button" class="accordion" id = "AccelerationFilter">Acceleration</button>
           <!-- Populates results using php -->
-          <form name="MPG_highway" method="post" action="index.php"  class="panel" id="HighwayMPGFilterOptions">
-            <ul>
-              <?php
-                PopulateSearchPanel("MPG_highway", $curr_query);
-              ?>
-            </ul>
-          </form>
-          <button class="accordion" id = "AccelerationFilter">Acceleration</button>
+          <ul>
+            <?php
+              echo PopulateSearchPanel("60_time", $curr_query);
+            ?>
+          </ul>
+          <button type="button" class="accordion" id = "DrivetrainFilter">Drivetrain</button>
           <!-- Populates results using php -->
-          <form name="60_time" method="post" action="index.php"  class="panel" id="AccelerationFilterOptions">
-            <ul>
-              <?php
-                PopulateSearchPanel("60_time", $curr_query);
-              ?>
-            </ul>
-          </form>
-          <button class="accordion" id = "DrivetrainFilter">Drivetrain</button>
+          <ul>
+            <?php
+              echo PopulateSearchPanel("DriveTrain_id", $curr_query);
+            ?>
+          </ul>
+          <!-- </form> -->
+          <button type="button" class="accordion" id = "OccupancyFilter">Occupancy</button>
           <!-- Populates results using php -->
-          <form name="DriveTrain_id" method="post" action="index.php"  class="panel" id="DrivetrainFilterOptions">
-            <ul>
-              <?php
-                PopulateSearchPanel("DriveTrain_id", $curr_query);
-              ?>
-            </ul>
-          </form>
-          <button class="accordion" id = "OccupancyFilter">Occupancy</button>
+          <ul>
+            <?php
+              echo PopulateSearchPanel("occupant_id", $curr_query);
+            ?>
+          </ul>
+          <button type="button" class="accordion" id = "CylinderFilter">Cylinder</button>
           <!-- Populates results using php -->
-          <form name= "occupant_id" method="post" action="index.php"  class="panel" id="OccupancyFilterOptions">
-            <ul>
-              <?php
-                PopulateSearchPanel("occupant_id", $curr_query);
-              ?>
-            </ul>
-          </form>
-          <button class="accordion" id = "CylinderFilter">Cylinder</button>
+          <ul>
+            <?php
+              echo PopulateSearchPanel("cylinder_id", $curr_query);
+            ?>
+          </ul>
+          <button type="button" class="accordion" id = "BodyStyleFilter">Body Style</button>
           <!-- Populates results using php -->
-          <form name="cylinder_id" method="post" action="index.php"  class="panel" id="CylinderFilterOptions">
-            <ul>
-              <?php
-                PopulateSearchPanel("cylinder_id", $curr_query);
-              ?>
-            </ul>
-          </form>
-          <button class="accordion" id = "BodyStyleFilter">Body Style</button>
+          <ul>
+            <?php
+              echo PopulateSearchPanel("BodyModel_id", $curr_query);
+            ?>
+          </ul>
+          <!-- </form> -->
+          <button type="button" class="accordion" id = "HorsepowerFilter">Horsepower</button>
           <!-- Populates results using php -->
-          <form name="BodyModel_id" method="post" action="index.php"  class="panel" id="BodyStyleFilterOptions">
-            <ul>
-              <?php
-                PopulateSearchPanel("BodyModel_id", $curr_query);
-              ?>
-            </ul>
-          </form>
-          <button class="accordion" id = "HorsepowerFilter">Horsepower</button>
-          <!-- Populates results using php -->
-          <form name="Horsepower_id" method="post" action="index.php"  class="panel" id="HorsePowerFilterOptions">
-            <ul>
-              <?php
-                PopulateSearchPanel("Horsepower_id", $curr_query);
-              ?>
-            </ul>
-          </form>
+          <ul>
+            <?php
+              echo PopulateSearchPanel("Horsepower_id", $curr_query);
+            ?>
+          </ul>
         </div>
       </div>
       <div class="in-line" id = "BodyResultsPanel">
@@ -184,7 +137,7 @@
             <!--####-->
             <!-- TO DO: Change value when query changes-->
             <?php
-              resultsPanelCount($curr_query);
+              //resultsPanelCount($curr_query);
             ?>
           </div>
           <div class="PanelHeaders">cars meet your requirements</div>
@@ -192,11 +145,7 @@
           <!--Dropdown for cars per page -->
           <div class="PanelHeaders">
             <div class="dropdown">
-              <!-- TO DO: Change Value of LIMIT for results in CarsResultContainer
-                  SQL Query: "LIMIT <value>"
-                -->
-              <!-- <button onclick="ResultsPerPageDropdown()" class="dropbtn">5</button> -->
-              <select id="ResultsPerPageDropdown" onchange="OrderByDropdownChange(this)">
+              <select name="ResultsPerPageDropdown" id="ResultsPerPageDropdown" onchange="ResultsPerPageDropdownChange(this)">
                 <option value="5">5</option>
                 <option value="10">10</option>
                 <option value="15">15</option>
@@ -209,11 +158,7 @@
           <!--Dropdown for sort by -->
           <div class="PanelHeaders">
             <div class="dropdown">
-              <!-- TO DO:  Change value of ORDER BY for results in CarsResultContainer
-                   SQL Query: "ORDER BY SortByDropdownValue"
-                -->
-              <!-- <button onclick="SortByDropdown()" class="dropbtn" value="make">Make</button> -->
-              <select id="SortByDropdown" onchange="SortByDropdownChange(this)">
+              <select name="SortByDropdown" id="SortByDropdown" onchange="SortByDropdownChange(this)">
                 <option selected value="make">Make</option>
                 <option value="model">Model</option>
                 <option value="year">Year</option>
@@ -230,31 +175,23 @@
             </div>
           </div>
         </div>
-        <hr/>
-        <!-- Populate using PHP -->
-        <div class = "CarResultContainer">
-          <div>
-            <?php
-              /* TO DO: Change to current query */
-              $query = "SELECT * FROM car ;";
-              resultsPanelPopulation($query);
-            ?>
-          </div>
-        </div>
+      </form>
+      <hr/>
+      <!-- Populate using PHP -->
+      <div class = "CarResultContainer">
+        <iframe name="results_panel" src='php/resultsPanelPopulation.php'></iframe>
       </div>
+    </div>
     </div>
     <!-- TO DO: Fix CSS (Note in Banner_Styles.css)-->
     <div id = "PageFooter">
       <hr/>
       <div id = "FooterText">This website is being used for educational purposes.</div>
     </div>
-
   </body>
 
   <!-- External javascripts -->
   <script src="./javascript/accordion.js"></script>
-  <script src="./javascript/checkbox.js"></script>
-
   <script>
     /* Internal Javascripts */
   </script>
