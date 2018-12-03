2e7e1b2c501c452337b18b55cee9679f014b3656
bhiggi12 <42845468+bhiggi12@users.noreply.github.com>
2018-12-02 21:02:03 -0500

    Updated resultsPanelPopulation.php
    Allows for different queries to be sent on submit

--- php/resultsPanelPopulation.php
+++ php/resultsPanelPopulation.php
@@ -1,8 +1,32 @@
 <!-- Populates the results panel using information from the database
      TO DO: Formatting of the car_result
   -->
-<?php
-  function resultsPanelPopulation($sql) {
+<!DOCTYPE html>
+  <html>
+  <head>
+    <title>Cars4Everyone_Results</title>
+
+    <!-- External Styling Sheets -->
+    <link href="./css/GeneralLayout.css" type="text/css" rel="stylesheet"/>
+    <link href="./css/ResultsPanelResults.css" type="text/css" rel="stylesheet"/>
+
+  </head>
+
+  <body>
+  <?php
+
+    include 'buildingQuery.php';
+    include 'formValidation.php';
+
+    if ($submit) {
+      // echo "Values Checked: ";
+      // print_r($valueschecked);
+      // echo "<br/> Results: " + $resultsperpagevalue + "<br/>" + $sortbyvalue;
+      $curr_query = currQuery($valueschecked, $sortbyvalue, $resultsperpagevalue);
+    } else {
+      $curr_query = "SELECT * from car";
+    }
+    // echo "Current Query: $curr_query";
 
     require('database.php');
 
@@ -14,8 +38,7 @@
     // echo "Printing ".$variable." values <br/>";
     //$sql = "SELECT * FROM car ;";
     // echo $sql."<br/>";
-    $result = $conn->query($sql);
-
+    $result = $conn->query($curr_query);
     if ($result->num_rows > 0) {
       // output data of each row
       while($row = $result->fetch_assoc()) {
@@ -35,22 +58,9 @@
         echo "</div>";
       }
     } else {
-        echo "0 results";
+        echo "No Cars To Display.  Try Changing Your Search.";
     }
     $conn->close();
-  }
 
-  function resultsPanelCount($sql) {
-    require('database.php');
-
-    $conn = new mysqli($servername, $username, $password, $dbname);
-    if ($conn->connect_errno) {
-      die('ERROR NO DATABSE');
-    }
-
-    $result = $conn->query($sql);
-    echo $result->num_rows;
-    $conn->close();
-  }
-
-?>
+  ?>
+</body>
