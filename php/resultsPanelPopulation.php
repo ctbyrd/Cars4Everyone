bf2cbaf08be2b471775fc4aa1bf606ea3a5270c4
bhiggi12 <42845468+bhiggi12@users.noreply.github.com>
2018-12-11 19:20:16 -0500

    Updated results panel to table format

--- php/resultsPanelPopulation.php
+++ php/resultsPanelPopulation.php
@@ -17,6 +17,7 @@
 
     include 'buildingQuery.php';
     include 'formValidation.php';
+    include 'results_style.php';
 
     if ($submit) {
       // echo "Values Checked: ";
@@ -42,20 +43,21 @@
     if ($result->num_rows > 0) {
       // output data of each row
       while($row = $result->fetch_assoc()) {
-        echo "<div class=\"car_result\">";
-        echo "<strong>Make: </strong>".$row["make"]."<br/>";
-        echo "<strong>Model: </strong>".$row['model']."<br/>";
-        echo "<strong>Year: </strong>".$row['year']."<br/>";
-        echo "<strong>Price: </strong>".$row['price']."<br/>";
-        echo "<strong>Body Style: </strong>".$row['BodyModel_id']."<br/>";
-        echo "<strong>Occupancy: </strong>".$row['occupant_id']."<br/>";
-        echo "<strong>City MPG: </strong>".$row['MPG_city']."<br/>";
-        echo "<strong>Highway MPG: </strong>".$row['MPG_highway']."<br/>";
-        echo "<strong>Acceleration: </strong>".$row['60_time']."<br/>";
-        echo "<strong>Cylinder: </strong>".$row['cylinder_id']."<br/>";
-        echo "<strong>Drivetrain: </strong>".$row['DriveTrain_id']."<br/>";
-        echo "<strong>Horsepower: </strong>".$row['Horsepower_id']."<br/>";
-        echo "</div>";
+        resultsTable($row);
+        // echo "<div class=\"car_result\">";
+        // echo "<strong>Make: </strong>".$row["make"]."<br/>";
+        // echo "<strong>Model: </strong>".$row['model']."<br/>";
+        // echo "<strong>Year: </strong>".$row['year']."<br/>";
+        // echo "<strong>Price: </strong>".$row['price']."<br/>";
+        // echo "<strong>Body Style: </strong>".$row['BodyModel_id']."<br/>";
+        // echo "<strong>Occupancy: </strong>".$row['occupant_id']."<br/>";
+        // echo "<strong>City MPG: </strong>".$row['MPG_city']."<br/>";
+        // echo "<strong>Highway MPG: </strong>".$row['MPG_highway']."<br/>";
+        // echo "<strong>Acceleration: </strong>".$row['60_time']."<br/>";
+        // echo "<strong>Cylinder: </strong>".$row['cylinder_id']."<br/>";
+        // echo "<strong>Drivetrain: </strong>".$row['DriveTrain_id']."<br/>";
+        // echo "<strong>Horsepower: </strong>".$row['Horsepower_id']."<br/>";
+        // echo "</div>";
       }
     } else {
         echo "No Cars To Display.  Try Changing Your Search.";
--- php/results_style.php
+++ php/results_style.php
@@ -1,38 +1,39 @@
 
 <?php
-
-  <table>
-  echo "<div class=\"car_result\">";
-  echo
-  "<tr>
-  <th> Make:</th>
-  <th> Model:</th>
-  <th> Year:</th>
-  <th> Price:</th>
-  <th> Body Style:</th>
-  <th> Occupancy:</th>
-  <th> MPG_City:</th>
-  <th> MPG_Highway:</th>
-  <th> 60s_Time:</th>
-  <th> #cyclinders:</th>
-  <th> Drivetrain:</th>
-  <th> Horsepower:</th>
-  </tr>";
-  echo "</div>";
-  echo "<tr>";
-  echo "<td>"".$row["make"]."</td><br/>";
-  echo "<td>"".$row['model'."</td><br/>";
-  echo "<td>"".$row["year"]."</td><br/>";
-  echo "<td>"".$row["price"]."</td><br/>";
-  echo "<td>"".$row["BodyModel_id"]."</td><br/>";
-  echo "<td>"".$row["occupant_id"]."</td><br/>";
-  echo "<td>"".$row["MPG_city"]."</td><br/>";
-  echo "<td>"".$row["MPG_highway"]."</td><br/>";
-  echo "<td>"".$row["60_time"]."</td><br/>";
-  echo "<td>"".$row["cylinder_id"]."</td><br/>";
-  echo "<td>"".$row["DriveTrain_id"]."</td><br/>";
-  echo "<td>"".$row["Horsepower_id"]."</td><br/>";
- echo "</td>";
-  echo "</tr>";
-  echo "</table>";
+  function resultsTable($row){
+    echo "<table>";
+    echo "<div class=\"car_result\">";
+    echo
+    "<tr>
+    <th> Make:</th>
+    <th> Model:</th>
+    <th> Year:</th>
+    <th> Price:</th>
+    <th> Body Style:</th>
+    <th> Occupancy:</th>
+    <th> MPG_City:</th>
+    <th> MPG_Highway:</th>
+    <th> 60s_Time:</th>
+    <th> #cyclinders:</th>
+    <th> Drivetrain:</th>
+    <th> Horsepower:</th>
+    </tr>";
+    echo "</div>";
+    echo "<tr>";
+    echo "<td>".$row["make"]."</td><br/>";
+    echo "<td>".$row['model']."</td><br/>";
+    echo "<td>".$row["year"]."</td><br/>";
+    echo "<td>".$row["price"]."</td><br/>";
+    echo "<td>".$row["BodyModel_id"]."</td><br/>";
+    echo "<td>".$row["occupant_id"]."</td><br/>";
+    echo "<td>".$row["MPG_city"]."</td><br/>";
+    echo "<td>".$row["MPG_highway"]."</td><br/>";
+    echo "<td>".$row["60_time"]."</td><br/>";
+    echo "<td>".$row["cylinder_id"]."</td><br/>";
+    echo "<td>".$row["DriveTrain_id"]."</td><br/>";
+    echo "<td>".$row["Horsepower_id"]."</td><br/>";
+    echo "</td>";
+    echo "</tr>";
+    echo "</table>";
+  }
 ?>
